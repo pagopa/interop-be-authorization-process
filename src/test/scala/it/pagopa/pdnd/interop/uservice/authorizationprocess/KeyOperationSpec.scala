6aee0a077ae026d4e482280cc391a93d3eabf243
galales <25105748+galales@users.noreply.github.com>
2021-09-08 18:04:21 +0200

    pin-415: Delete key tests

--- src/test/scala/it/pagopa/pdnd/interop/uservice/authorizationprocess/KeyOperationSpec.scala
+++ src/test/scala/it/pagopa/pdnd/interop/uservice/authorizationprocess/KeyOperationSpec.scala
@@ -83,6 +83,40 @@ class KeyOperationSpec extends AnyWordSpecLike with MockFactory with SpecUtils w
     }
   }
 
+  "Delete key" should {
+    "succeed" in {
+      val kid = "some-kid"
+      (mockAuthorizationManagementService.deleteKey _)
+        .expects(createdClient.id, kid)
+        .once()
+        .returns(Future.successful(()))
+
+      Get() ~> service.deleteClientKeyById(createdClient.id.toString, kid) ~> check {
+        status shouldEqual StatusCodes.NoContent
+      }
+    }
+
+    "fail if missing authorization header" in {
+      implicit val contexts: Seq[(String, String)] = Seq.empty[(String, String)]
+      val kid                                      = "some-kid"
+      Get() ~> service.deleteClientKeyById(createdClient.id.toString, kid) ~> check {
+        status shouldEqual StatusCodes.Unauthorized
+      }
+    }
+
+    "fail if client or key do not exist" in {
+      val kid = "some-kid"
+      (mockAuthorizationManagementService.deleteKey _)
+        .expects(*, *)
+        .once()
+        .returns(Future.failed(keymanagement.client.invoker.ApiError(404, "message", None)))
+
+      Get() ~> service.deleteClientKeyById(createdClient.id.toString, kid) ~> check {
+        status shouldEqual StatusCodes.NotFound
+      }
+    }
+  }
+
   "Enable key" should {
     "succeed" in {
       val kid = "some-kid"
