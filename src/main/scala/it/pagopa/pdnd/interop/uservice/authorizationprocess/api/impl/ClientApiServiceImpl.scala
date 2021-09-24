a0a602b011962d989490cb89bcd908638131ca68
galales <25105748+galales@users.noreply.github.com>
2021-09-24 11:34:38 +0200

    pin-541-fix: Fix party tax code not found error

--- src/main/scala/it/pagopa/pdnd/interop/uservice/authorizationprocess/api/impl/ClientApiServiceImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/authorizationprocess/api/impl/ClientApiServiceImpl.scala
@@ -471,12 +471,13 @@ class ClientApiServiceImpl(
   ): Future[Option[partymanagement.client.model.Relationship]] =
     for {
       consumer <- partyManagementService.getOrganization(consumerId)
-      relationships <- recoverIfMissing(
+      relationships <-
         partyManagementService
           .getRelationships(consumer.institutionId, taxCode, PartyManagementService.ROLE_SECURITY_OPERATOR)
-          .map(Some(_)),
-        Future.successful(None)
-      )
+          .map(Some(_))
+        // TODO This is dangerous because every error is treated as "missing party with given tax code"
+        //  but currently there is no precise way to identify the error
+          .recoverWith(_ => Future.successful(None))
       activeRelationships = relationships.toSeq.flatMap(_.items.filter(_.status == RelationshipEnums.Status.Active))
       securityOperatorRel = activeRelationships.headOption // Only one expected
     } yield securityOperatorRel
