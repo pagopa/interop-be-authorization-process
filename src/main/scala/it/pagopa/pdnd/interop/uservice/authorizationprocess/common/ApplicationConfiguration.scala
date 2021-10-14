11cba14a7f383dcc29f6ecb07f21f651c3f08889
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2021-10-14 09:36:01 +0200

    move services version in application.conf

--- src/main/scala/it/pagopa/pdnd/interop/uservice/authorizationprocess/common/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/authorizationprocess/common/ApplicationConfiguration.scala
@@ -14,25 +14,13 @@ object ApplicationConfiguration {
     s"$keyManagementUrl/pdnd-interop-uservice-key-management/0.0.1"
   }
 
-  def getAgreementManagementURL: String = {
-    val agreementProcessURL: String = config.getString("services.agreement-management")
-    s"$agreementProcessURL/pdnd-interop-uservice-agreement-management/0.0.1"
-  }
+  def getAgreementManagementURL: String = config.getString("services.agreement-management")
 
-  def getCatalogManagementURL: String = {
-    val catalogManagementURL: String = config.getString("services.catalog-management")
-    s"$catalogManagementURL/pdnd-interop-uservice-catalog-management/0.0.1"
-  }
+  def getCatalogManagementURL: String = config.getString("services.catalog-management")
 
-  def getAuthorizationManagementURL: String = {
-    val authorizationManagementURL: String = config.getString("services.key-management")
-    s"$authorizationManagementURL/pdnd-interop-uservice-key-management/0.0.1"
-  }
+  def getAuthorizationManagementURL: String = config.getString("services.key-management")
 
-  def getPartyManagementURL: String = {
-    val partyManagementURL: String = config.getString("services.party-management")
-    s"$partyManagementURL/pdnd-interop-uservice-party-management/0.0.1"
-  }
+  def getPartyManagementURL: String = config.getString("services.party-management")
 
   def getPdndIdIssuer: String = {
     config.getString("uservice-authorization-process.issuer")
