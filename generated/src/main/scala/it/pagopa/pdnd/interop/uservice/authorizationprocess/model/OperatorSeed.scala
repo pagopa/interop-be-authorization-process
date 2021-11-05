92535045f22c3a01c29803f8328432bba230db40
P3trur0 <ubaldopescatore@gmail.com>
2021-11-05 17:55:30 +0100

    restored generated code on gitignore

--- generated/src/main/scala/it/pagopa/pdnd/interop/uservice/authorizationprocess/model/OperatorSeed.scala
+++
@@ -1,9 +0,0 @@
-package it.pagopa.pdnd.interop.uservice.authorizationprocess.model
-
-/** Models the seed for an Operator-Client relationship to be persisted
-  *
-  * @param taxCode  for example: ''null''
-  * @param name  for example: ''null''
-  * @param surname  for example: ''null''
-  */
-final case class OperatorSeed(taxCode: String, name: String, surname: String)
