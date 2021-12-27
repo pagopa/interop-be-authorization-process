d530750f42576d8b66179e0196d7f3d4c6689540
P3trur0 <ubaldopescatore@gmail.com>
2021-12-27 11:09:14 +0100

    replacing openapi4j with swagger-request-validator

--- project/Versions.scala
+++ project/Versions.scala
@@ -13,10 +13,9 @@ object Versions {
   lazy val mockitoVersion           = "3.9.0"
   lazy val mustacheVersion          = "0.9.10"
   lazy val nimbusVersion            = "9.15.2"
-  lazy val openapi4jVersion         = "1.0.7"
   lazy val scalaMockVersion         = "5.1.0"
   lazy val scalatestVersion         = "3.2.10"
-  lazy val stateMonitoringVersion   = "0.0.3-SNAPSHOT"
+  lazy val swaggerValidatorVersion  = "2.23.0"
 }
 
 object PDNDVersions {
