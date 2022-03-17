a243a5dd8865915957e325f70bd2aa2b1fc6a546
P3trur0 <ubaldopescatore@gmail.com>
2022-03-17 18:21:56 +0100

    bump version

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,7 +19,7 @@ object Versions {
 
 object PagopaVersions {
   lazy val agreementManagementVersion     = "0.1.1"
-  lazy val catalogManagementVersion       = "0.1.1"
+  lazy val catalogManagementVersion       = "0.1.x-SNAPSHOT"
   lazy val commonsVersion                 = "0.1.1"
   lazy val authorizationManagementVersion = "0.1.1"
   lazy val partyManagementVersion         = "0.1.7"
