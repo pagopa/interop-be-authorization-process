11960c7ba4476b0dd11b50a316204eb4b74992da
Scala Steward <scala-steward@pagopa.it>
2022-10-01 06:50:30 +0000

    Update nimbus-jose-jwt to 9.25.4 in 1.0.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   lazy val json4sVersion    = "4.0.4"
   lazy val logbackVersion   = "1.2.11"
   lazy val mustacheVersion  = "0.9.10"
-  lazy val nimbusVersion    = "9.23"
+  lazy val nimbusVersion    = "9.25.4"
   lazy val scalaMockVersion = "5.2.0"
   lazy val scalatestVersion = "3.2.12"
 }
