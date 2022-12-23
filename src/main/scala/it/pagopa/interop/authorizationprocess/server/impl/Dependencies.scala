1fa7f3f2db3a1a08ba75a2e01ab93f10ceb36e32
Antonio Gelameris <toniogela89@gmail.com>
2022-12-23 12:57:10 +0100

    Changed logging (#194)
    * Changed logging
    
    * Update Dependencies.scala

--- src/main/scala/it/pagopa/interop/authorizationprocess/server/impl/Dependencies.scala
+++ src/main/scala/it/pagopa/interop/authorizationprocess/server/impl/Dependencies.scala
@@ -40,8 +40,13 @@ import it.pagopa.interop.selfcare.userregistry.client.api.{UserApi => UserRegist
 import it.pagopa.interop.selfcare.userregistry.client.invoker.ApiKeyValue
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import com.typesafe.scalalogging.{Logger, LoggerTakingImplicit}
+import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
 
 trait Dependencies {
+
+  implicit val loggerTI: LoggerTakingImplicit[ContextFieldsToLog] =
+    Logger.takingImplicit[ContextFieldsToLog]("OAuth2JWTValidatorAsContexts")
 
   implicit val partyManagementApiKeyValue: PartyManagementApiKeyValue = PartyManagementApiKeyValue()
 
@@ -132,7 +137,7 @@ trait Dependencies {
   ): OperatorApi = new OperatorApi(
     OperatorApiServiceImpl(authorizationManagementService(blockingEc), partyManagementService()),
     OperatorApiMarshallerImpl,
-    jwtReader.OAuth2JWTValidatorAsContexts
+    jwtReader.OAuth2JWTValidatorAsContexts(Logger.takingImplicit[ContextFieldsToLog]("OAuth2JWTValidatorAsContexts"))
   )
 
   def getJwtValidator(): Future[JWTReader] = JWTConfiguration.jwtReader
