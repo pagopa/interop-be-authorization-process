d3a8e49d5207a789f6840ed00b67b124309f14fc
Riccardo Torsoli <riccardo.torsoli@emea.nttdata.com>
2023-01-19 14:29:54 +0100

    PIN-2633 User authorizations in routes

---
+++ src/main/scala/it/pagopa/interop/authorizationprocess/common/AuthorizationUtils.scala
@@ -0,0 +1,35 @@
+package it.pagopa.interop.authorizationprocess.common
+
+import cats.implicits._
+import it.pagopa.interop.commons.utils.AkkaUtils.getOrganizationIdFutureUUID
+import it.pagopa.interop.authorizationprocess.error.AuthorizationProcessErrors._
+import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
+import it.pagopa.interop.authorizationprocess.service.AuthorizationManagementService
+
+object AuthorizationUtils {
+
+  def assertIsConsumer(clientId: UUID)(
+    authorizationManagementService: AuthorizationManagementService
+  )(implicit contexts: Seq[(String, String)], ec: ExecutionContext): Future[Unit] =
+    for {
+      organizationId <- getOrganizationIdFutureUUID(contexts)
+      _              <- authorizationManagementService
+        .getClient(clientId)(contexts)
+        .ensureOr(client => OrganizationNotAllowedOnClient(clientId.toString, client.consumerId))(
+          _.consumerId == organizationId
+        )
+    } yield ()
+
+  def assertIsConsumerOfRequestedPurpose(purposeId: UUID, consumerId: UUID)(implicit
+    contexts: Seq[(String, String)],
+    ec: ExecutionContext
+  ): Future[Unit] =
+    for {
+      organizationId <- getOrganizationIdFutureUUID(contexts)
+      _              <- Future
+        .failed(OrganizationNotAllowedOnPurpose(purposeId.toString, consumerId.toString))
+        .unlessA(consumerId == organizationId)
+
+    } yield ()
+}
