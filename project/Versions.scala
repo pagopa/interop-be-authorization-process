08e84736667389f1f17077e85fa40110cb75eecc
Stefano Perazzolo <13318704+beetlecrunch@users.noreply.github.com>
2023-05-05 11:11:57 +0200

    Revert "Update pagopa versions"
    This reverts commit a47b5befeb969884d8d2b06da94798f0c216b062.

--- project/Versions.scala
+++ project/Versions.scala
@@ -16,12 +16,12 @@ object Versions {
 }
 
 object PagopaVersions {
-  lazy val agreementManagementVersion     = "1.0.10"
-  lazy val catalogManagementVersion       = "1.0.12"
-  lazy val commonsVersion                 = "1.0.16"
-  lazy val authorizationManagementVersion = "1.0.11"
-  lazy val tenantManagementVersion        = "1.0.6"
-  lazy val partyManagementVersion         = "1.0.5"
-  lazy val purposeManagementVersion       = "1.0.8"
-  lazy val userRegistryManagementVersion  = "1.0.5"
+  lazy val agreementManagementVersion     = "1.0.x-SNAPSHOT"
+  lazy val catalogManagementVersion       = "1.0.x-SNAPSHOT"
+  lazy val commonsVersion                 = "1.0.x-SNAPSHOT"
+  lazy val authorizationManagementVersion = "1.0.x-SNAPSHOT"
+  lazy val tenantManagementVersion        = "1.0.x-SNAPSHOT"
+  lazy val partyManagementVersion         = "1.0.x-SNAPSHOT"
+  lazy val purposeManagementVersion       = "1.0.x-SNAPSHOT"
+  lazy val userRegistryManagementVersion  = "1.0.x-SNAPSHOT"
 }
