ac698241446ddfc6728e0cc24b7914f939d69351
Riccardo Torsoli <riccardo.torsoli@nttdata.com>
2023-07-09 17:10:49 +0200

    PIN-3495 Use Management KeyProcessor

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,6 +30,8 @@ object Dependencies {
       namespace %% "interop-be-authorization-management-client" % authorizationManagementVersion
     lazy val authorizationManagementModels =
       namespace %% "interop-be-authorization-management-models" % authorizationManagementVersion
+    lazy val authorizationManagementProcessor =
+      namespace %% "interop-be-authorization-management-processor" % authorizationManagementVersion
     lazy val agreementManagementModels =
       namespace %% "interop-be-agreement-management-models" % agreementManagementVersion
     lazy val catalogManagementModels =
@@ -137,6 +139,7 @@ object Dependencies {
       pagopa.commonsCqrs                    % Compile,
       pagopa.authorizationManagementClient  % Compile,
       pagopa.authorizationManagementModels  % Compile,
+      pagopa.authorizationManagementProcessor  % Compile,
       pagopa.partyManagement                % Compile,
       pagopa.purposeManagementModels        % Compile,
       pagopa.tenantManagementModels         % Compile,
--- src/main/scala/it/pagopa/interop/authorizationprocess/common/Adapters.scala
+++ src/main/scala/it/pagopa/interop/authorizationprocess/common/Adapters.scala
@@ -1,6 +1,7 @@
 package it.pagopa.interop.authorizationprocess.common
 
 import it.pagopa.interop.authorizationprocess.model._
+import it.pagopa.interop.authorizationprocess.model.{KeyUse => ProcessKeyUse}
 import it.pagopa.interop.authorizationmanagement.model.client._
 import it.pagopa.interop.authorizationmanagement.model.client.PersistentClientComponentState.Active
 import it.pagopa.interop.authorizationmanagement.model.client.PersistentClientComponentState.Inactive
@@ -8,12 +9,13 @@ import it.pagopa.interop.authorizationmanagement.model.client.{Api, Consumer}
 import it.pagopa.interop.authorizationmanagement.model.key.{Enc, Sig}
 import it.pagopa.interop.authorizationmanagement.client.{model => AuthorizationManagementDependency}
 import it.pagopa.interop.authorizationprocess.common.readmodel.model.ReadModelClientWithKeys
-import it.pagopa.interop.authorizationmanagement.model.key.PersistentKey
-import it.pagopa.interop.authorizationprocess.service.impl.KeyProcessor
+import it.pagopa.interop.authorizationmanagement.model.key.{PersistentKeyUse, PersistentKey}
+import it.pagopa.interop.authorizationmanagement.processor.key.KeyProcessor
+import com.nimbusds.jose.jwk._
+import scala.jdk.CollectionConverters.{ListHasAsScala, SetHasAsScala}
+import scala.annotation.nowarn
 
 import java.util.UUID
-import it.pagopa.interop.authorizationmanagement.model.key.PersistentKeyUse
-
 import java.time.OffsetDateTime
 
 object Adapters {
@@ -60,8 +62,7 @@ object Adapters {
         relationshipId = k.relationshipId
       )
     def toReadKeyApi(op: Operator): Either[Throwable, ReadClientKey] =
-      KeyProcessor
-        .fromBase64encodedPEMToAPIKey(k.kid, k.encodedPem, k.use, k.algorithm)
+      fromBase64encodedPEMToAPIKey(k.kid, k.encodedPem, k.use, k.algorithm)
         .map(key =>
           ReadClientKey(
             name = k.name,
@@ -72,8 +73,7 @@ object Adapters {
         )
 
     def toClientKeyApi: Either[Throwable, ClientKey] =
-      KeyProcessor
-        .fromBase64encodedPEMToAPIKey(k.kid, k.encodedPem, k.use, k.algorithm)
+      fromBase64encodedPEMToAPIKey(k.kid, k.encodedPem, k.use, k.algorithm)
         .map(key => ClientKey(name = k.name, createdAt = k.createdAt, key = key))
   }
 
@@ -165,19 +165,19 @@ object Adapters {
       )
   }
 
-  implicit class KeyUseWrapper(private val use: KeyUse) extends AnyVal {
+  implicit class KeyUseWrapper(private val use: ProcessKeyUse) extends AnyVal {
     def toDependency: AuthorizationManagementDependency.KeyUse = use match {
-      case KeyUse.SIG => AuthorizationManagementDependency.KeyUse.SIG
-      case KeyUse.ENC => AuthorizationManagementDependency.KeyUse.ENC
+      case ProcessKeyUse.SIG => AuthorizationManagementDependency.KeyUse.SIG
+      case ProcessKeyUse.ENC => AuthorizationManagementDependency.KeyUse.ENC
     }
   }
 
   implicit class PersistentKeyUseWrapper(private val use: PersistentKeyUse) extends AnyVal {
-    def toApi: KeyUse      = use match {
-      case Sig => KeyUse.SIG
-      case Enc => KeyUse.ENC
+    def toApi: ProcessKeyUse = use match {
+      case Sig => ProcessKeyUse.SIG
+      case Enc => ProcessKeyUse.ENC
     }
-    def toRfcValue: String = use match {
+    def toRfcValue: String   = use match {
       case Sig => "sig"
       case Enc => "enc"
     }
@@ -275,4 +275,145 @@ object Adapters {
       case AuthorizationManagementDependency.ClientComponentState.INACTIVE => ClientComponentState.INACTIVE
     }
   }
+
+  def fromBase64encodedPEMToAPIKey(
+    kid: String,
+    base64PEM: String,
+    use: PersistentKeyUse,
+    algorithm: String
+  ): Either[Throwable, Key] = {
+    val key = for {
+      jwk <- KeyProcessor.fromBase64encodedPEM(base64PEM)
+    } yield jwk.getKeyType match {
+      case KeyType.RSA => rsa(kid, jwk.toRSAKey)
+      case KeyType.EC  => ec(kid, jwk.toECKey)
+      case KeyType.OKP => okp(kid, jwk.toOctetKeyPair)
+      case KeyType.OCT => oct(kid, jwk.toOctetSequenceKey)
+      case _           => throw new RuntimeException(s"Unknown KeyType ${jwk.getKeyType}")
+    }
+
+    key.map(_.copy(alg = Some(algorithm), use = Some(use.toRfcValue)))
+  }
+
+  private def rsa(kid: String, key: RSAKey): Key           = {
+    val otherPrimes = Option(key.getOtherPrimes)
+      .map(list =>
+        list.asScala
+          .map(entry =>
+            OtherPrimeInfo(
+              r = entry.getPrimeFactor.toString,
+              d = entry.getFactorCRTExponent.toString,
+              t = entry.getFactorCRTCoefficient.toString
+            )
+          )
+          .toSeq
+      )
+      .filter(_.nonEmpty)
+
+    Key(
+      use = None,
+      alg = None,
+      kty = key.getKeyType.getValue,
+      key_ops = Option(key.getKeyOperations).map(list => list.asScala.map(op => op.toString).toSeq),
+      kid = kid,
+      x5u = Option(key.getX509CertURL).map(_.toString),
+      x5t = getX5T(key),
+      x5tS256 = Option(key.getX509CertSHA256Thumbprint).map(_.toString),
+      x5c = Option(key.getX509CertChain).map(list => list.asScala.map(op => op.toString).toSeq),
+      crv = None,
+      x = None,
+      y = None,
+      d = Option(key.getPrivateExponent).map(_.toString),
+      k = None,
+      n = Option(key.getModulus).map(_.toString),
+      e = Option(key.getPublicExponent).map(_.toString),
+      p = Option(key.getFirstPrimeFactor).map(_.toString),
+      q = Option(key.getSecondPrimeFactor).map(_.toString),
+      dp = Option(key.getFirstFactorCRTExponent).map(_.toString),
+      dq = Option(key.getSecondFactorCRTExponent).map(_.toString),
+      qi = Option(key.getFirstCRTCoefficient).map(_.toString),
+      oth = otherPrimes
+    )
+  }
+  private def ec(kid: String, key: ECKey): Key             = Key(
+    use = None,
+    alg = None,
+    kty = key.getKeyType.getValue,
+    key_ops = Option(key.getKeyOperations).map(list => list.asScala.map(op => op.toString).toSeq),
+    kid = kid,
+    x5u = Option(key.getX509CertURL).map(_.toString),
+    x5t = getX5T(key),
+    x5tS256 = Option(key.getX509CertSHA256Thumbprint).map(_.toString),
+    x5c = Option(key.getX509CertChain).map(list => list.asScala.map(op => op.toString).toSeq),
+    crv = Option(key.getCurve).map(_.toString),
+    x = Option(key.getX).map(_.toString),
+    y = Option(key.getY).map(_.toString),
+    d = Option(key.getD).map(_.toString),
+    k = None,
+    n = None,
+    e = None,
+    p = None,
+    q = None,
+    dp = None,
+    dq = None,
+    qi = None,
+    oth = None
+  )
+  private def okp(kid: String, key: OctetKeyPair): Key     = {
+    Key(
+      use = None,
+      alg = None,
+      kty = key.getKeyType.getValue,
+      key_ops = Option(key.getKeyOperations).map(list => list.asScala.map(op => op.toString).toSeq),
+      kid = kid,
+      x5u = Option(key.getX509CertURL).map(_.toString),
+      x5t = getX5T(key),
+      x5tS256 = Option(key.getX509CertSHA256Thumbprint).map(_.toString),
+      x5c = Option(key.getX509CertChain).map(list => list.asScala.map(op => op.toString).toSeq),
+      crv = Option(key.getCurve).map(_.toString),
+      x = Option(key.getX).map(_.toString),
+      y = None,
+      d = Option(key.getD).map(_.toString),
+      k = None,
+      n = None,
+      e = None,
+      p = None,
+      q = None,
+      dp = None,
+      dq = None,
+      qi = None,
+      oth = None
+    )
+  }
+  private def oct(kid: String, key: OctetSequenceKey): Key = {
+    Key(
+      use = None,
+      alg = None,
+      kty = key.getKeyType.getValue,
+      key_ops = Option(key.getKeyOperations).map(list => list.asScala.map(op => op.toString).toSeq),
+      kid = kid,
+      x5u = Option(key.getX509CertURL).map(_.toString),
+      x5t = getX5T(key),
+      x5tS256 = Option(key.getX509CertSHA256Thumbprint).map(_.toString),
+      x5c = Option(key.getX509CertChain).map(list => list.asScala.map(op => op.toString).toSeq),
+      crv = None,
+      x = None,
+      y = None,
+      d = None,
+      k = Option(key.getKeyValue).map(_.toString),
+      n = None,
+      e = None,
+      p = None,
+      q = None,
+      dp = None,
+      dq = None,
+      qi = None,
+      oth = None
+    )
+
+  }
+
+  // encapsulating in a method to avoid compilation errors because of Nimbus deprecated method
+  @nowarn
+  @inline private def getX5T(key: JWK): Option[String] = Option(key.getX509CertThumbprint).map(_.toString)
 }
