ec02a5e7bce4682e01a2ac17cf9ab195e1fef405
nttdata-rtorsoli <riccardo.torsoli@emeal.nttdata.com>
2023-10-16 09:14:01 +0200

    PIN-3516 Cleaning

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,7 +19,7 @@ object PagopaVersions {
   lazy val agreementManagementVersion     = "1.0.x-SNAPSHOT"
   lazy val catalogManagementVersion       = "1.0.x-SNAPSHOT"
   lazy val commonsVersion                 = "1.0.x-SNAPSHOT"
-  lazy val authorizationManagementVersion = "1.0.x-SNAPSHOT"
+  lazy val authorizationManagementVersion = "PIN-4112-SNAPSHOT/"//"1.0.x-SNAPSHOT"
   lazy val tenantManagementVersion        = "1.0.x-SNAPSHOT"
   lazy val partyManagementVersion         = "1.0.x-SNAPSHOT"
   lazy val purposeManagementVersion       = "1.0.x-SNAPSHOT"
